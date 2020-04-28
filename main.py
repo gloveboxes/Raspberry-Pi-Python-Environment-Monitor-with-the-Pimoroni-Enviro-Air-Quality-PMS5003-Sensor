56f1ba1f78ab4b65a188a23b947cfc9c045a6b6f
Dave Glover <dglover@microsoft.com>
2020-04-28 09:12:55 +0100

    refactored

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 import time
-from pms5003 import PMS5003, ReadTimeoutError
 import json
-import os
 import asyncio
 from azure.iot.device.aio import IoTHubDeviceClient
 import sys
-import psutil
+from device_provisioning_service import Device
+from sensor import Sensor
 
 async def main():
 
-    avg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    samples = 15
+    dps = Device("0ne0002EE6D", "RPiEnviroPlus", "VIfFoVJz8xCmqr/pPGpW3ofCAFl53vA16MNtIXPsLM0=")
 
-    conn_str = os.getenv("IOTHUB_DEVICE_CONNECTION_STRING")
-    print(conn_str)
+    conn_str = await dps.connection_string
+
     device_client = IoTHubDeviceClient.create_from_connection_string(conn_str)
     await device_client.connect()
 
-    pms5003 = PMS5003()
-    time.sleep(1.0)
-
-    pid = os.getpid()
-    py = psutil.Process(pid)
-
+    sensor = Sensor()
+    
     async def message_listener(device_client):
         while True:
             message = await device_client.receive_message()  # blocking call
@@ -36,35 +30,16 @@ async def main():
     try:
         while True:
             try:
-                avg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+                telemetry = sensor.readSensor()
+                if telemetry is not None:
 
-                for i in range(samples):
-                    readings = pms5003.read()
-                    for j in range(14):
-                        avg[j] += readings.data[j]
-                    time.sleep(1)
+                    print(telemetry)
+                    data = json.dumps(telemetry)
 
-                telemetry = {
-                    # PM1.0 ug/m3 (ultrafine particles)
-                    "PM1-0": int(avg[0]/samples),
-                    # PM2.5 ug/m3 (combustion particles, organic compounds, metals)
-                    "PM2-5": int(avg[1]/samples),
-                    # PM10 ug/m3  (dust, pollen, mould spores)
-                    "PM10": int(avg[2]/samples),
-                    "Mem": py.memory_info()[0]
-                }
+                    await device_client.send_message(data)
 
-                print(telemetry)
+                    time.sleep(5)
 
-                data = json.dumps(telemetry)
-
-                await device_client.send_message(data)
-
-                time.sleep(60*10)
-
-            except ReadTimeoutError:
-                pms5003 = PMS5003()
-                print('Timeout reading PMS5003 Sensor')
             except:
                 print("Unexpected error:", sys.exc_info()[0])
 
---
+++ sensor.py
@@ -0,0 +1,56 @@
+from pms5003 import PMS5003, ReadTimeoutError
+from bme280 import BME280
+try:
+    from smbus2 import SMBus
+except ImportError:
+    from smbus import SMBus
+import psutil
+import os
+import time
+
+
+class Sensor():
+
+    def __init__(self):
+        self.pms5003 = PMS5003()
+        self.bus = SMBus(1)
+        self.bme280 = BME280(i2c_dev=self.bus)
+
+        self.pid = os.getpid()
+        self.py = psutil.Process(self.pid)
+
+        # pause for a moment to allow the pms5003 to settle
+        time.sleep(1.0)
+
+
+    def readSensor(self):
+        samples = 15
+        avg = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        telemetry = None
+
+        try:
+            for i in range(samples):
+                readings = self.pms5003.read()
+                for j in range(14):
+                    avg[j] += readings.data[j]
+                time.sleep(1)
+
+            telemetry = {
+                # PM1.0 ug/m3 (ultrafine particles)
+                "PM1-0": int(avg[0]/samples),
+                # PM2.5 ug/m3 (combustion particles, organic compounds, metals)
+                "PM2-5": int(avg[1]/samples),
+                # PM10 ug/m3  (dust, pollen, mould spores)
+                "PM10": int(avg[2]/samples),
+                "Temperature" : round(self.bme280.get_temperature(), 1),
+                "Pressure": round(self.bme280.get_pressure(), 1),
+                "Humidity" : round(self.bme280.get_humidity(), 1),
+                "Mem": self.py.memory_info()[0]
+            }
+
+        
+        except ReadTimeoutError:
+            self.pms5003 = PMS5003()
+            print('Timeout reading PMS5003 Sensor')
+
+        return telemetry
